a32c99b939e6b79a098429dc505d12bf0a2924ac
Quan - Manh NGUYEN <manhquan03tn@gmail.com>
2025-05-02 15:36:50 +0700

    Update trocr.py

--- trocr.py
+++ trocr.py
@@ -1,22 +1,22 @@
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
-import torch
-from PIL import Image
+import os
 import cv2
-import pytesseract
+import torch
+import tempfile
 import numpy as np
+import pandas as pd
+from PIL import Image
 import streamlit as st
 from datetime import datetime
-import pandas as pd
-import os
-import tempfile
-from models_ import yolo_v9c, yolo_v10x
+from models_ import yolo_v9c, yolo_v10x, yolo_v11n
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 # Initialize TrOCR model and processor
 processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-printed")
+#model = VisionEncoderDecoderModel.from_pretrained("/Users/dolphin/Downloads/smart-parking/TrOCR_base/model_checkpoint")
 model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-printed")
-
+#model = VisionEncoderDecoderModel.from_pretrained("/Users/dolphin/Downloads/smart-parking/TrOCR_base")
 # Path to the CSV file
-csv_file_path = 'parking_records.csv'
+csv_file_path = '/Users/dolphin/Downloads/smart-parking/parking_records.csv'
 
 # Load or initialize the parking_df DataFrame
 try:
@@ -30,21 +30,26 @@ def preprocess_image(img):
     img = cv2.resize(img, None, fx=2.0, fy=2.0, interpolation=cv2.INTER_CUBIC)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.fastNlMeansDenoising(img, None, 30, 7, 21)
-    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                cv2.THRESH_BINARY, 11, 2)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
     smooth = cv2.GaussianBlur(img, (1, 1), 0)
     return smooth
 
 # Function to detect plate using YOLO model
-def detect_objects(img, conf=0.8, model_type='YOLOv9C'):
-    model = yolo_v9c.YOLOv9C(r"/Users/dolphin/Downloads/projet/anpr/YOLO/best_v9c.pt") if model_type == 'YOLOv9C' else yolo_v10x.YOLOv10X(r"/Users/dolphin/Downloads/projet/anpr/YOLO/best_v10x.pt")
+def detect_objects(img, conf=0.8, model_type='YOLOv11N'):
+    if model_type == 'YOLOv11N':
+        model = yolo_v11n.YOLOv11N(r"/Users/dolphin/Downloads/smart-parking/YOLO/best_yolo11n_aug_weights.pt")
+    elif model_type == 'YOLOv10X':
+        model = yolo_v10x.YOLOv10X(r"/Users/dolphin/Downloads/smart-parking/YOLO/best_v10x.pt")
+    else:
+        model = yolo_v9c.YOLOv9C(r"/Users/dolphin/Downloads/smart-parking/YOLO/best_v9c.pt")
+
     results, output_image = model.detect(img, conf)
     return results, output_image
 
 # Function to perform OCR using TrOCR
-def ocr_trocr(img):
+def transformer_ocr(img):
     pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # Convert OpenCV image to PIL
     pixel_values = processor(pil_img, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
@@ -62,20 +67,17 @@ def check_in(plate_number):
 def check_out(plate_number):
     global parking_df
     check_out_time = datetime.now()
-    
-    # Update check-out time
     parking_df.loc[(parking_df['plate_number'] == plate_number) & (parking_df['check_out'].isnull()), 'check_out'] = check_out_time
 
     # Calculate duration and fee
     check_in_time = pd.to_datetime(parking_df.loc[(parking_df['plate_number'] == plate_number) & (parking_df['check_out'] == check_out_time), 'check_in'].values[0])
     duration_hours = (check_out_time - check_in_time).total_seconds() / 3600
-    fee = round(duration_hours * 15000, 2)  # Fee per hour = 15000 VND
+    fee = round(duration_hours * 100000, 2)
 
     # Save duration and fee into the DataFrame
     parking_df.loc[(parking_df['plate_number'] == plate_number) & (parking_df['check_out'] == check_out_time), 'duration_hours'] = duration_hours
     parking_df.loc[(parking_df['plate_number'] == plate_number) & (parking_df['check_out'] == check_out_time), 'fee_vnd'] = fee
-
-    # Save the updated DataFrame to CSV
+    
     parking_df.to_csv(csv_file_path, index=False)
 
     return duration_hours, fee
@@ -96,20 +98,22 @@ def handle_input():
     # Handle image input
     if input_type == 'Image':
         uploaded_file = st.file_uploader("Upload an image to process",
-                                          type=["jpg", "jpeg", "png"],
-                                          help="Supported image formats: JPG, JPEG, PNG")
+                                          type=["jpg", "jpeg", "png", "webp"],
+                                          help="Supported image formats: JPG, JPEG, PNG, WEBP")
 
         if uploaded_file is not None:
             file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
             image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            st.sidebar.image(image, caption="UPLOADED IMAGE", use_column_width=True)
+
+            st.sidebar.image(image, caption="UPLOADED IMAGE", use_container_width=True)
             return image
 
     # Handle video input
     elif input_type == 'Video':
         uploaded_video = st.file_uploader("Upload a video file to process", 
-                                          type=["mp4", "avi", "mov"])
+                                          type=["mp4", "avi", "mov", "webm"],
+                                          help="Supported video formats: MP4, AVI, MOV, WEBM")
 
         if uploaded_video is not None:
             tfile = tempfile.NamedTemporaryFile(delete=False)
@@ -118,13 +122,13 @@ def handle_input():
             st.video(tfile.name)
 
             frames_processed = 0
-            frame_skip = 50
-            stop_processing = False  # Flag to stop processing once a valid plate is detected
-            detected_plate_text = None  # Store detected plate
+            frame_skip = 80
+            stop_processing = False  # Boolean flag
+            detected_plate_text = None
 
             # Iterate through video frames
             while video_capture.isOpened():
-                if stop_processing:  # Stop processing if condition is met
+                if stop_processing:
                     break
 
                 success, frame = video_capture.read()
@@ -133,7 +137,7 @@ def handle_input():
 
                 if frames_processed % frame_skip == 0:
                     # Process each frame for plate detection
-                    plates, output_image = detect_objects(frame, conf=0.5, model_type="YOLOv9C")
+                    plates, output_image = detect_objects(frame, conf=0.5, model_type="YOLOv11N")
 
                     # Collect results for all frames and perform character recognition (OCR)
                     if plates:
@@ -141,14 +145,14 @@ def handle_input():
                             x1, y1, x2, y2, conf = plate
                             # Crop the detected plate from the frame
                             cropped_plate = output_image[y1:y2, x1:x2]
-                            st.image(cropped_plate, caption="Detected Plate", use_column_width=True)
+                            st.image(cropped_plate, caption="Detected Plate", use_container_width=True)
                             
                             # Preprocess the cropped plate image for better OCR accuracy
                             processed_plate = preprocess_image(cropped_plate)
-                            st.image(processed_plate, caption="Processed Plate for OCR", use_column_width=True)
+                            st.image(processed_plate, caption="Processed Plate for OCR", use_container_width=True)
                             
                             # Perform OCR to extract characters
-                            plate_text = ocr_trocr(processed_plate)
+                            plate_text = transformer_ocr(processed_plate)
                             st.write(f"Detected License Plate: {plate_text}")
 
                             # If a plate with exactly 8 alphanumeric characters is detected, save it and stop processing
@@ -175,14 +179,14 @@ def handle_input():
             # Release the video_capture resource after processing the entire video
             video_capture.release()
 
-            # Display final detected plate (if any) and the CSV data
+            # Display final detected plate and the CSV data
             if detected_plate_text:
                 st.write(f"Final Detected License Plate: {detected_plate_text}")
 
             st.write(f"Total Frames Processed: {frames_processed}")
 
             # Display the contents of the CSV file
-            st.subheader("Parking Records")
+            st.subheader("Records Management")
             csv_data = pd.read_csv(csv_file_path)
             st.dataframe(csv_data)
             
@@ -193,19 +197,20 @@ def handle_input():
         cap = cv2.VideoCapture(0)  # Open the default webcam
 
         if cap.isOpened():
-            st.write("Webcam is active!")
+            st.write("Your Macbook's camera is active!")
             frames_processed = 0
-            frame_skip = 50
+            frame_skip = 80
             stop_processing = False
             detected_plate_text = None
+            detected_frame = None
 
             while True:
                 ret, frame = cap.read()
                 if not ret:
-                    st.error("Failed to read frame from webcam.")
+                    st.error("Failed to read frame from webcam!")
                     break
 
-                st.image(frame, channels="BGR", caption="Webcam Stream", use_column_width=True)
+                st.image(frame, channels="BGR", caption="Webcam Stream", use_container_width=True)
 
                 if frames_processed % frame_skip == 0:
                     plates, output_image = detect_objects(frame, conf=0.5, model_type="YOLOv9C")
@@ -213,10 +218,10 @@ def handle_input():
                         for plate in plates:
                             x1, y1, x2, y2, conf = plate
                             cropped_plate = output_image[y1:y2, x1:x2]
-                            st.image(cropped_plate, caption="Detected Plate", use_column_width=True)
+                            st.image(cropped_plate, caption="Detected Plate", use_container_width=True)
                             processed_plate = preprocess_image(cropped_plate)
-                            st.image(processed_plate, caption="Processed Plate for OCR", use_column_width=True)
-                            plate_text = ocr_trocr(processed_plate)
+                            st.image(processed_plate, caption="Processed Plate for OCR", use_container_width=True)
+                            plate_text = transformer_ocr(processed_plate)
                             st.write(f"Detected License Plate: {plate_text}")
                             is_valid, clean_plate = is_valid_plate(plate_text)
 
@@ -258,29 +263,28 @@ def handle_input():
 
 # Main function
 def main():
-    st.set_page_config(page_title="ANPR using TrOCR", page_icon="✨", layout="centered", initial_sidebar_state="expanded")
+    st.set_page_config(page_title="Smart Parking system", page_icon="✨", layout="centered", initial_sidebar_state="expanded")
     st.title('Automatic Number Plate Recognition With Microsoft\'s TrOCR🚘🚙')
     st.sidebar.title('Settings 😎')
-    conf = st.sidebar.slider("Confidence Threshold:", 0.0, 1.0, 0.5, step=0.05)
-    model_type = st.sidebar.selectbox("Select Model", ("YOLOv9C", "YOLOv10X"))
-    # Get input from the user
+    conf = st.sidebar.slider("Confidence Threshold:", 0.0, 1.0, 0.8, step=0.05)
+    model_type = st.sidebar.selectbox("Select Model", ("YOLOv9C", "YOLOv10X", "YOLOv11N"))
+
     image = handle_input()
 
     if image is not None:
-        # Detect plates using YOLO
-        plates, output_image = detect_objects(image, conf=0.5, model_type="YOLOv9C")
-
+        plates, output_image = detect_objects(image, conf=0.5, model_type="YOLOv11N")
         if len(plates) > 0:
             for plate in plates:
                 x1, y1, x2, y2, conf = plate
                 # Crop the detected plate from the image
                 cropped_plate = output_image[y1:y2, x1:x2]
-                
+
                 # Preprocess the cropped plate image for better OCR accuracy
                 processed_img = preprocess_image(cropped_plate)
-                st.sidebar.image(processed_img, caption="Cropped Plate for OCR", use_column_width=True)
+                st.sidebar.image(processed_img, caption="Cropped Plate for OCR", use_container_width=True)
+
                 # Perform OCR to extract characters
-                plate_text = ocr_trocr(processed_img)
+                plate_text = transformer_ocr(processed_img)
 
                 # Check if the plate number already exists in the CSV and if it's checked out
                 existing_record = parking_df[(parking_df['plate_number'] == plate_text) & (parking_df['check_out'].isnull())]
@@ -293,7 +297,6 @@ def main():
                     check_in(plate_text)
 
                 # Show plate text and confidence in sidebar
-                # st.sidebar.text(f'  Plate detected: {plate_text}\n  Confidence: {conf:.2f}')
                 cv2.rectangle(output_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(output_image, plate_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
@@ -301,9 +304,9 @@ def main():
             st.subheader("No License Plates Detected !")
 
         # Display output image
-        st.image(output_image, caption="PROCESSED IMAGE", use_column_width=True)
+        st.image(output_image, caption="PROCESSED IMAGE", use_container_width=True)
 
-        # Display the contents of the CSV file
+        # Display CSV file
         st.subheader("Parking Records")
         csv_data = pd.read_csv(csv_file_path)
         st.dataframe(csv_data)
